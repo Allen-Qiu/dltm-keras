20eca776634504f0bb657a27ed63074153470124
Allen-Qiu <jiangtaoqiu@gmail.com>
2020-09-14 20:09:57 +0800

    Add files via upload

---
+++ keras-mul.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 19 22:34:54 2020
+
+@author: Allen
+"""
+# mlp for multiclass classification
+from numpy import argmax
+from pandas import read_csv
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense
+# load the dataset
+path = 'D:/qjt/beike/deeplearning/2020/iris.txt'
+df = read_csv(path, header=None)
+# split into input and output columns
+X, y = df.values[:, :-1], df.values[:, -1]
+# ensure all data are floating point values
+X = X.astype('float32')
+# encode strings to integer
+y = LabelEncoder().fit_transform(y)
+# split into train and test datasets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
+print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+# determine the number of input features
+n_features = X_train.shape[1]
+# define model
+model = Sequential()
+model.add(Dense(10, activation='relu', kernel_initializer='he_normal', input_shape=(n_features,)))
+model.add(Dense(8, activation='relu', kernel_initializer='he_normal'))
+model.add(Dense(3, activation='softmax'))
+# compile the model
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+# fit the model
+model.fit(X_train, y_train, epochs=300, batch_size=32, verbose=0)
+# evaluate the model
+loss, acc = model.evaluate(X_test, y_test, verbose=0)
+print('Test Accuracy: %.3f' % acc)
+# make a prediction
+row = [5.1,3.5,1.4,0.2]
+yhat = model.predict([row])
+print('Predicted: %s (class=%d)' % (yhat, argmax(yhat)))
