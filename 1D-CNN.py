a0a5958d3c30914c7f8aa5c92b684893d7922ab1
Allen-Qiu <jiangtaoqiu@gmail.com>
2020-09-26 22:08:51 +0800

    Add files via upload

---
+++ 1D-CNN.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  8 20:10:46 2020
+
+human activity recognize
+
+
+@author: Allen
+"""
+# cnn model
+from numpy import mean
+from numpy import std
+from numpy import dstack
+from pandas import read_csv
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Input
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import Conv1D
+from tensorflow.keras.layers import MaxPooling1D
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import Model
+
+# load a single file as a numpy array
+def load_file(filepath):
+	dataframe = read_csv(filepath, header=None, delim_whitespace=True)
+	return dataframe.values
+
+# load a list of files and return as a 3d numpy array
+def load_group(filenames, prefix=''):
+	loaded = list()
+	for name in filenames:
+		data = load_file(prefix + name)
+		loaded.append(data)
+	# stack group so that features are the 3rd dimension
+	loaded = dstack(loaded)
+	return loaded
+
+# load a dataset group, such as train or test
+def load_dataset_group(group, prefix=''):
+	filepath = prefix + group + '/Inertial Signals/'
+	# load all 9 files as a single array
+	filenames = list()
+	# total acceleration
+	filenames += ['total_acc_x_'+group+'.txt', 'total_acc_y_'+group+'.txt', 'total_acc_z_'+group+'.txt']
+	# body acceleration
+	filenames += ['body_acc_x_'+group+'.txt', 'body_acc_y_'+group+'.txt', 'body_acc_z_'+group+'.txt']
+	# body gyroscope
+	filenames += ['body_gyro_x_'+group+'.txt', 'body_gyro_y_'+group+'.txt', 'body_gyro_z_'+group+'.txt']
+	# load input data
+	X = load_group(filenames, filepath)
+	# load class output
+	y = load_file(prefix + group + '/y_'+group+'.txt')
+	return X, y
+
+# load the dataset, returns train and test X and y elements
+def load_dataset(prefix=''):
+	# load all train
+	trainX, trainy = load_dataset_group('train', prefix + 'HARDataset/')
+	print(trainX.shape, trainy.shape)
+	# load all test
+	testX, testy = load_dataset_group('test', prefix + 'HARDataset/')
+	print(testX.shape, testy.shape)
+	# zero-offset class values
+	trainy = trainy - 1
+	testy = testy - 1
+	# one hot encode y
+	trainy = to_categorical(trainy)
+	testy = to_categorical(testy)
+	print(trainX.shape, trainy.shape, testX.shape, testy.shape)
+	return trainX, trainy, testX, testy
+
+
+
+# hyper-parameters
+verbose, epochs, batch_size = 0, 10, 32
+trainX, trainy, testX, testy = load_dataset('D:/qjt/beike/deeplearning/2020/')
+
+n_timesteps, n_features, n_outputs = trainX.shape[1], trainX.shape[2], trainy.shape[1]
+
+model = Sequential()
+model.add(Conv1D(filters=64, kernel_size=3, activation='relu', input_shape=(n_timesteps,n_features)))
+model.add(Dropout(0.5))
+model.add(MaxPooling1D(pool_size=2))
+model.add(Flatten())
+model.add(Dense(100, activation='relu'))
+model.add(Dense(n_outputs, activation='softmax'))
+
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.fit(trainX, trainy, epochs=epochs, batch_size=batch_size, verbose=verbose)
+_, accuracy = model.evaluate(testX, testy, batch_size=batch_size, verbose=0)
+
+score = accuracy * 100.0
+print('accuracy: %.3f' % (score))
+
+for layer in model.layers:
+    print(layer.output_shape)
---
+++ mnist-cnn.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  8 14:38:13 2020
+
+@author: Allen
+"""
+
+from tensorflow.keras.layers import Input, Dense, Conv2D, MaxPool2D, Flatten, Dropout, Reshape
+from tensorflow.keras import Sequential
+import tensorflow as tf
+
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+n,m = x_train.shape[1], x_train.shape[2]
+
+model = Sequential()
+model.add(Reshape((n,m,1), input_shape=(n,m)))
+model.add(Conv2D(filters=32, kernel_size=5, activation='relu', padding="same"))
+model.add(MaxPool2D())
+model.add(Conv2D(filters=64, kernel_size=5, activation='relu', padding="same"))
+model.add(MaxPool2D())
+model.add(Flatten())
+model.add(Dense(1024,activation='relu', kernel_initializer='he_normal'))
+model.add(Dropout(0.5))
+model.add(Dense(10, activation='softmax'))
+
+loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+model.compile(optimizer='Nadam',
+              loss=loss_fn,
+              metrics=['accuracy'])
+model.fit(x_train, y_train, epochs=10, batch_size=100)
+model.evaluate(x_test,  y_test, verbose=2)
+
+for layer in model.layers:
+    print(('%s - %s')%(layer.input_shape, layer.output_shape))
