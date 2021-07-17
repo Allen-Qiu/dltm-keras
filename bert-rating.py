643fbc4107e7cdb3f4c3c8391614ad886750eada
Jiangtao Qiu <jiangtaoqiu@gmail.com>
2021-07-17 15:59:33 +0800

    Add files via upload

--- bert-rating.py
+++ bert-rating.py
@@ -1,9 +1,28 @@
-import json
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  9 09:47:58 2021
+
+predicting ratings of reviews in yelp
+
+@author: qjt16
+"""
+import numpy as np
 import tensorflow as tf
+import json
+
 from transformers import BertTokenizer, TFBertModel
 from transformers import TFAutoModel
+from transformers import TFAutoModelForSequenceClassification
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.layers import Average, Dense, Input, GlobalAveragePooling1D
 
-fin = open('yelp-reviews-small.json')
+# 0. hyperparameters ----------------
+seq_len = 500
+batch_size = 10
+epoch = 3
+local = "../model/bert-base-uncased"
+# 1. dataset ---------
+fin = open('../data/yelp-reviews.json')
 lines = fin.readlines()
 reviews = list()
 stars = list()
@@ -12,39 +31,61 @@ for line in lines:
     rdict = json.loads(line)
     reviews.append(rdict['text'])
     stars.append(rdict['stars'])
+steps = max([len(line) for line in reviews])
 
-local = r"C:\Users\qjt16\Desktop\bert-base-uncased"
-checkpoint = "bert-base-uncased"
+np.random.seed(10)
+shuffle_indices = np.random.permutation(np.arange(len(stars)))
+
+x_shuffled = np.array(reviews)[shuffle_indices]
+y_shuffled = np.array(stars)[shuffle_indices]
+
+# Split train/test set
+x_train, x_test = x_shuffled[:-1000], x_shuffled[-1000:]
+y_train, y_test = y_shuffled[:-1000], y_shuffled[-1000:]
+
+N_CLASSES = len(set(y_train))
+
+# 2. prepare inputs for bert ----------
 tokenizer = BertTokenizer.from_pretrained(local)
-classifier = TFAutoModel.from_pretrained(local)
 
-inputs = tokenizer(reviews, padding=True, max_length=512, truncation=True, return_tensors="tf")
-outputs = classifier(inputs)
-predictions = tf.math.softmax(outputs.logits, axis=-1)
+encodings = tokenizer(x_train.tolist(), truncation=True, padding='max_length', max_length=seq_len)
+X_train = [np.array(encodings["input_ids"]),
+           np.array(encodings["token_type_ids"]),
+           np.array(encodings["attention_mask"])]
 
+encodings = tokenizer(x_test.tolist(), truncation=True, padding='max_length', max_length=seq_len)
+X_test = [np.array(encodings["input_ids"]),
+          np.array(encodings["token_type_ids"]),
+          np.array(encodings["attention_mask"])]
 
-k = 0
-for review, star in zip(reviews,stars):
-    try:
-        r=classifier(
-            review
-        )
-        print("predict:%s, label:%s"%(r[0]['label'], star))
-        if k>100: break
-        k += 1
-    except:
-        None
-#%%
-# import tensorflow as tf
-# from transformers import AutoTokenizer
-# from transformers import TFAutoModel
-# from transformers import TFAutoModelForSequenceClassification
+# 3. build model ---------------
+strategy = tf.distribute.MirroredStrategy()
+print('Number of devices: {}'.format(strategy.num_replicas_in_sync))
 
-# checkpoint = "distilbert-base-uncased-finetuned-sst-2-english"
-# checkpoint = r"C:\Users\qjt16\Desktop\bert-base-uncased"
-# tokenizer = AutoTokenizer.from_pretrained(checkpoint)
-# classifier = TFAutoModelForSequenceClassification.from_pretrained(checkpoint)
+with strategy.scope():
+    input_ids = Input(shape=(seq_len,), dtype=tf.int32, name='input_ids')
+    input_type = Input(shape=(seq_len,), dtype=tf.int32, name='token_type_ids')
+    input_mask = Input(shape=(seq_len,), dtype=tf.int32, name='attention_mask')
+    inputs = [input_ids, input_type, input_mask]
 
-# inputs = tokenizer(reviews, padding=True, truncation=True, return_tensors="tf", max_length=512)
-# outputs = classifier(inputs)
-# predictions = tf.math.softmax(outputs.logits, axis=-1)
+    bert = TFBertModel.from_pretrained(local)
+    bert_outputs = bert(inputs)
+    last_hidden_states = bert_outputs.last_hidden_state
+    avg = GlobalAveragePooling1D()(last_hidden_states)
+    output = Dense(1)(avg)
+    # output = Dense(N_CLASSES, activation="softmax")(avg)
+    opt = tf.keras.optimizers.Adam(learning_rate=3e-5)
+    model = Model(inputs=inputs, outputs=output)
+    rmse=tf.keras.metrics.RootMeanSquaredError()
+    model.compile(loss='mse', optimizer=opt, metrics=['mae',rmse])
+    # model.compile(loss='sparse_categorical_crossentropy', optimizer=opt, metrics=['accuracy','mse'])
+
+model.summary()
+
+# 3. fine-tunning --------------------
+model.fit(X_train, y_train, epochs=epoch, batch_size=batch_size, verbose=1)
+
+model.evaluate(X_test,y_test,verbose=1)
+res=model.predict(X_test)
+for i in range(len(y_test)):
+    print("%s, %s"%(res[i], y_test[i]))
